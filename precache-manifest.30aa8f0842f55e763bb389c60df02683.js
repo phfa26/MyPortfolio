f1691e327b8e1380220531ad552187c362f9208e
phfa26 <phfa@outlook.com.au>
2020-03-07 21:53:33 +1100

    Updates

---
+++ precache-manifest.30aa8f0842f55e763bb389c60df02683.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62980955f58fbe04e90b9c754e57420e",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "8f4243bbed4637439682",
+    "url": "/MyPortfolio/static/css/main.c012a782.chunk.css"
+  },
+  {
+    "revision": "9f93f51d00761849ac79",
+    "url": "/MyPortfolio/static/js/2.fc56847c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MyPortfolio/static/js/2.fc56847c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4243bbed4637439682",
+    "url": "/MyPortfolio/static/js/main.98dd39b2.chunk.js"
+  },
+  {
+    "revision": "1ee184eb1dec9096b718",
+    "url": "/MyPortfolio/static/js/runtime-main.e4667418.js"
+  },
+  {
+    "revision": "01b40d88cbc252d9f9f5f59bc24df99b",
+    "url": "/MyPortfolio/static/media/ExpensesTrackerApp.01b40d88.png"
+  },
+  {
+    "revision": "2b9316d6623ab2ab79fba2a0573333b4",
+    "url": "/MyPortfolio/static/media/GivngApp.2b9316d6.png"
+  },
+  {
+    "revision": "65a233a35eb76d126636f6b458f1dfe3",
+    "url": "/MyPortfolio/static/media/MockedIn.65a233a3.png"
+  },
+  {
+    "revision": "44df9778ab823566e1c5cab51a91b346",
+    "url": "/MyPortfolio/static/media/Portfolio.44df9778.png"
+  },
+  {
+    "revision": "52dd46b32bc11b5b29391ced5fb6a378",
+    "url": "/MyPortfolio/static/media/TicTacToe.52dd46b3.png"
+  },
+  {
+    "revision": "c1b8608dbf1cde7b8632186d9a2316d0",
+    "url": "/MyPortfolio/static/media/pic.c1b8608d.jpeg"
+  }
+]);
