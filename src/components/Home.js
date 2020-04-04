2df1539be8fae8f129c916cf326ceebfdcbde09b
phfa26 <phfa@outlook.com.au>
2020-04-04 15:07:23 +1100

    updated job title to jr dev

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 import "./Home.css";
-const Home = props => {
+const Home = (props) => {
   return (
     <div className="home-content">
       <div className="home-content__text">
         <h1 className="home-content__text-name">Paulo Amaral</h1>
-        <p>Junior Front-end Web Developer</p>
+        <p>Junior Developer</p>
       </div>
     </div>
   );
